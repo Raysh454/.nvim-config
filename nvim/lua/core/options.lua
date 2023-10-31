cbb504136c7a3d2cf83c9c0be6ff8c563dfbfec1
Raysh454 <67965243+Raysh454@users.noreply.github.com>
2023-10-31 19:01:23 +0500

    Add files via upload

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,18 @@
+vim.opt.number = true
+vim.opt.mouse = 'a'
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
